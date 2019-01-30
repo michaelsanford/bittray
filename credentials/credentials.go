ec8bdc2a046f1e67f41ffc6cb23a3bb2a1696446
Michael Sanford <michaelsanford@users.noreply.github.com>
2019-01-29 22:40:39 -0500

    Implement Windows Credential Manager

--- credentials/credentials.go
+++ credentials/credentials.go
@@ -1,29 +1,34 @@
-package main
+package credentials
 
 import (
 	"fmt"
 	"github.com/danieljoos/wincred"
 )
 
+const credentialTarget string = "github.com/michaelsanford/bittray"
+
 func StoreCred(url string, username string, password string) {
-	cred := wincred.NewGenericCredential("bittray")
-	cred.CredentialBlob = []byte(username + "|" + url + "|" + password)
+	cred := wincred.NewGenericCredential(credentialTarget)
+	cred.UserName = username
+	cred.Comment = url
+	cred.CredentialBlob = []byte(password)
 	err := cred.Write()
 
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 }
 
 func GetCred() {
-	cred, err := wincred.GetGenericCredential("bittray")
+	cred, err := wincred.GetGenericCredential(credentialTarget)
 	if err == nil {
-		fmt.Println(string(cred.CredentialBlob))
+		output := fmt.Sprintf("%s:%s@%s", cred.UserName, cred.CredentialBlob, cred.Comment)
+		fmt.Println(output)
 	}
 }
 
 func DestroyCred() {
-	cred, err := wincred.GetGenericCredential("bittray")
+	cred, err := wincred.GetGenericCredential(credentialTarget)
 	if err != nil {
 		fmt.Println(err)
 		return
