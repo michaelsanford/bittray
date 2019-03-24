408c7168bb135664c31c39470217246715b6c66f
Michael Sanford <michaelsanford@users.noreply.github.com>
2019-03-24 11:45:55 -0400

    Add comments reported by golint

--- config/constants.go
+++ config/constants.go
@@ -1,8 +1,10 @@
 package config
 
 var (
+	// GitHub URL to the Release API (for update checks)
 	GhAPI   = "https://api.github.com/repos/michaelsanford/bittray/releases/latest"
-	DocsURL = "https://michaelsanford.github.io/bittray/"
-
+	// The git tag of the current version
 	CurrentVersionTag = "1.1.0"
+	// GitHub URL to the documentation site
+	DocsURL = "https://michaelsanford.github.io/bittray/"
 )
