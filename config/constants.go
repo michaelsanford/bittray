75b8afc0003cd69319074501ed1f85e4d2311772
Michael Sanford <michaelsanford@users.noreply.github.com>
2019-03-24 11:48:35 -0400

    Fix comment style for golint

--- config/constants.go
+++ config/constants.go
@@ -1,10 +1,11 @@
 package config
 
 var (
-	// GitHub URL to the Release API (for update checks)
-	GhAPI   = "https://api.github.com/repos/michaelsanford/bittray/releases/latest"
-	// The git tag of the current version
+	// GhAPI is the GitHub URL to the Release API (for update checks)
+	GhAPI = "https://api.github.com/repos/michaelsanford/bittray/releases/latest"
+	// CurrentVersionTag is the git tag of the current version
 	CurrentVersionTag = "1.1.0"
-	// GitHub URL to the documentation site
+
+	// DocsURL is the GitHub URL to the documentation site
 	DocsURL = "https://michaelsanford.github.io/bittray/"
 )
